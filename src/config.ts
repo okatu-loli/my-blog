59a6d5a2dc6409b8a9eb87c0fef3a58eda74169e
okatu-loli <CN_QianShi@hotmail.com>
2024-09-06 23:58:32 +0800

    更新导航栏

--- src/config.ts
+++ src/config.ts
@@ -39,6 +39,11 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Archive,
     LinkPreset.About,
     {
+      name: '说说',
+      url: 'https://zone.cnqs.moe',     // Internal links should not include the base path, as it is automatically added
+      external: true,                               // Show an external link icon and will open in a new tab
+    },
+    {
       name: 'GitHub',
       url: 'https://github.com/okatu-loli',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
